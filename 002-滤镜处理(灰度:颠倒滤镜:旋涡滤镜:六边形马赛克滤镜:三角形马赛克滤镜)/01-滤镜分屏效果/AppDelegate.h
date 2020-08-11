c7020e6b8189c1fb7a495c1fc241ba62a6d2c51e
裴留振 <853415458@qq.com>
2020-08-12 02:31:00 +0800

    滤镜马赛克效果

---
+++ 002-滤镜处理(灰度:颠倒滤镜:旋涡滤镜:六边形马赛克滤镜:三角形马赛克滤镜)/01-滤镜分屏效果/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  01-滤镜分屏效果
+//
+//  Created by plz on 2020/8/8.
+//  Copyright © 2020 plz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 002-滤镜处理(灰度:颠倒滤镜:旋涡滤镜:六边形马赛克滤镜:三角形马赛克滤镜)/01-滤镜分屏效果/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  01-滤镜分屏效果
+//
+//  Created by plz on 2020/8/8.
+//  Copyright © 2020 plz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
